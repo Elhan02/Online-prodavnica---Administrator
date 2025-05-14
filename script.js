f08c2502cb3c1551b7c595c239068a53e3f3af9d
Elhan002 <elhanhajrovic02@gmail.com>
2025-05-14 20:53:27 +0200

    Ubacena logika za renderovanje tabele

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 class Product {
-    Product (name, price, description){
-        this.name = name
-        this.price = price
-        this.description = description
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+}
+
+let products = [
+                    new Product("TV", 350, '60" Smart televizor'),
+                    new Product("Toster", 30, 'smart toster high class')
+               ];
+
+function createProductRows(){
+    let table = document.querySelector("#products-body")
+
+    for (i = 0; i < products.length; i++){
+        let tr = document.createElement("tr")
+        let id = document.createElement("td")
+        let name = document.createElement("td")
+        let price = document.createElement("td")
+
+        id.textContent = i+1
+        name.textContent = products[i].name
+        price.textContent = products[i].price
+
+        tr.appendChild(id)
+        tr.appendChild(name)
+        tr.appendChild(price)
+
+        table.appendChild(tr)
     }
 }
 
-let products = []
-
+createProductRows()

70afb76bdb9e4ec9fc3521bd6655f5e696d70e52
Elhan Hajrovic <elhanhajrovic02@gmail.com>
2025-05-15 00:19:14 +0200

    Odradjen div sa detaljima o proizvodu na klik.

--- script.js
+++ script.js
@@ -7,29 +7,52 @@ class Product {
 }
 
 let products = [
-                    new Product("TV", 350, '60" Smart televizor'),
-                    new Product("Toster", 30, 'smart toster high class')
-               ];
+  new Product("TV", 350, '60" Smart televizor'),
+  new Product("Toster", 30, "smart toster high class"),
+];
 
-function createProductRows(){
-    let table = document.querySelector("#products-body")
+function createProductRows() {
+  let table = document.querySelector("#products-body");
 
-    for (i = 0; i < products.length; i++){
-        let tr = document.createElement("tr")
-        let id = document.createElement("td")
-        let name = document.createElement("td")
-        let price = document.createElement("td")
+  for (let i = 0; i < products.length; i++) {
+    let tr = document.createElement("tr");
+    let id = document.createElement("td");
+    let name = document.createElement("td");
+    let price = document.createElement("td");
 
-        id.textContent = i+1
-        name.textContent = products[i].name
-        price.textContent = products[i].price
+    id.textContent = i + 1;
+    name.textContent = products[i].name;
+    price.textContent = products[i].price;
 
-        tr.appendChild(id)
-        tr.appendChild(name)
-        tr.appendChild(price)
+    tr.addEventListener("click", function () {
+      productDetails(products[i]);
+    });
 
-        table.appendChild(tr)
-    }
+    tr.appendChild(id);
+    tr.appendChild(name);
+    tr.appendChild(price);
+    table.appendChild(tr);
+  }
 }
 
-createProductRows()
+function productDetails(product) {
+  let detailsP = document.createElement("p");
+  detailsP.innerHTML =
+    "Naziv: " +
+    product.name +
+    "<br>" +
+    "Cena: " +
+    product.price +
+    "<br>" +
+    "Opis: " +
+    product.description;
+
+  let detailsDiv = document.querySelector(".product-details");
+  if (detailsDiv.firstChild) {
+    detailsDiv.firstChild.remove();
+  }
+
+  detailsDiv.appendChild(detailsP);
+}
+
+document.addEventListener('DOMContentLoaded', createProductRows())

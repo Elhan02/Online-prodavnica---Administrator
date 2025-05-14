1c474d18277f077750f93a809c6de1a2a6f2c912
Elhan Hajrovic <elhanhajrovic02@gmail.com>
2025-05-14 18:39:55 +0200

    Dodat pocetni kod

---
+++ script.js
@@ -0,0 +1,10 @@
+class Product {
+    Product (name, price, description){
+        this.name = name
+        this.price = price
+        this.description = description
+    }
+}
+
+let products = []
+
